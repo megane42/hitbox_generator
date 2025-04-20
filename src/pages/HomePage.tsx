e8d5e4630fe7f8efc9fcbe8b318ff080cfd75223
megane42 <h.kzm.gm@gmail.com>
2025-04-20 21:16:13 +0900

    connect image select buttons with canvas

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import ImageCanvas from '@/components/ImageCanvas';
 import CameraOpenButton from "@/components/CameraOpenButton";
 import ImageSelectButton from '@/components/ImageSelectButton';
 
-const onImageGiven = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const image = event.target.files?.[0];
-  console.log(image?.size);
-};
-
 const HomePage = () => {
+  const DEFAULT_IMAGE = "https://picsum.photos/200/300";
+
+  const [imageUrl, setImageUrl] = useState<string>(DEFAULT_IMAGE);
+
+  const onImageGiven = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const image = event.target.files?.[0];
+
+    if (!image || !image.type.startsWith('image/')) {
+      alert('Selected file is not a valid image.');
+      return;
+    }
+
+    setImageUrl(URL.createObjectURL(image));
+  };
+
   return (
     <div>
       <h1>Hitbox Generator</h1>
       <section>
-        <ImageCanvas imageUrl="https://picsum.photos/200/300" />
+        <ImageCanvas imageUrl={imageUrl} />
       </section>
       <ImageSelectButton text="Choose from Device" onImageSelect={onImageGiven} />
       <CameraOpenButton  text="Take a Photo"       onImageTaken={onImageGiven} />
